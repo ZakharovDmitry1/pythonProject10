e6014de4743d90034dd370f10bf858c196022d10
ZakharovDmitry1 <vbnzvbnz1234567890@gmail.com>
2023-04-28 17:44:00 +0300

    add simple code4

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ from app.data import db_session
 
 def main():
     db_session.global_init("app/db/blogs.db")
-    port = int(os.environ.get("PORT", 5000))
-    serve(app, port=port, host='0.0.0.0')
+    app.run(port=8080, host='127.0.0.1')
 
 
 if __name__ == '__main__':
